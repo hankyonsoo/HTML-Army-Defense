ea6092545df70e3007cbe3eb2f2d69fe46e95f21
teamoo24 <601107w@gmail.com>
2019-08-13 16:22:12 +0900

    pngとjpeg生成

--- js/canvas.js
+++ js/canvas.js
@@ -1,10 +1,15 @@
-var cnvs = $(`<canvas>`).attr("width",300).attr("height",300).appendTo("body").get(0);
+var cnvs = $(`<canvas width=300 height=300>`).get(0)
 var cntxt = cnvs.getContext("2d")
 var w = cnvs.width;
 var h = cnvs.height;
 
-cntxt.fillStyle = "#ffffff";
-cntxt.fillRect(0, 0, w, h);
-
 cntxt.fillStyle = "#ff0000";
 cntxt.fillRect(w*0.1,h*0.1,w*0.8,h*0.8)
+
+var img = new Image();
+img.src = cnvs.toDataURL();
+$(img).appendTo("body");
+
+var img2 = new Image();
+img2.src = cnvs.toDataURL("image/jpeg",0.1);
+$(img2).appendTo("body");
